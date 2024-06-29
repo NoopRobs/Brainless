6d8549fd32d967a4203f3b3ca62ff0297b7b0df7
NoopRobs <150570960+NoopRobs@users.noreply.github.com>
2024-06-29 13:29:29 +0800

    Update Main.sh

--- Main.sh
+++ Main.sh
@@ -201,6 +201,10 @@ cmd power set-mode 0
 cmd thermalservice override-status 0
 cmd looper_stats disable
 
+if $launch ; then
+    results+=($(launch_app))   
+fi
+
 
 if pgrep -f "$game" > /dev/null;then
    am clear-watch-heap $game 
