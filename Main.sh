4c91338b2911e18bd35a4fa4517032abebbb41a6
NoopRobs <150570960+NoopRobs@users.noreply.github.com>
2024-07-06 13:57:05 +0700

    Update Main.sh

--- Main.sh
+++ Main.sh
@@ -3,6 +3,7 @@
 cd $(dirname $0)
 dos2unix data
 source data
+source function.sh
 
 found_packages=()
 for package in "${pkg[@]}"; do
@@ -23,22 +24,6 @@ size=$(wm size)
 density=$(wm density)
 width=$(echo $size | cut -d 'x' -f 1)
 height=$(echo $size | cut -d 'x' -f 2)
-
-
-launch_app () {
-   am start -D -N -S --user 0 "${id[0]}" --es --windowingMode 1 --no-window-animation --abi ARMEABI-V7A --splashscreen-icon
-      if [ $? -eq 0 ]; then
-         cmd notification post -S bigtext -t 'MLQL · Laxeron' 'Executed' 'Starting Mobile Legends with Armeabi-v7a !' > /dev/null 2>&1 &
-     else
-         am start -D -N -S --user 0 "${id[0]}" --es --windowingMode 1 --no-window-animation
-             if [ $? -eq 0 ]; then
-                cmd notification post -S bigtext -t 'MLQL · Laxeron' 'Executed' 'Starting APP, Enjoy your games !' > /dev/null 2>&1 &
-             else
-                echo "[ Can't start app or Error ! ]"
-             fi
-     fi
-}
-launch=true
 
 
 echo "[ Game Discovered as > $game ]"
