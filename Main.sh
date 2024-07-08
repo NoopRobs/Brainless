0c855b74e33d4528418e9252cf7156d873ab320e
NoopRobs <150570960+NoopRobs@users.noreply.github.com>
2024-07-08 10:02:57 +0700

    Update Main.sh

--- Main.sh
+++ Main.sh
@@ -3,7 +3,7 @@
 cd $(dirname $0)
 dos2unix data
 source data
-source function.sh
+
 
 found_packages=()
 for package in "${pkg[@]}"; do
@@ -25,6 +25,26 @@ density=$(wm density)
 width=$(echo $size | cut -d 'x' -f 1)
 height=$(echo $size | cut -d 'x' -f 2)
 
+
+
+launch_app=() {
+if ls /sdcard/android/data/$game/files/dragon2017/assets/comlibs/armeabi-v7a; then
+   am start -D -N -S --user 0 "${id[0]}" --es --windowingMode 1 --no-window-animation --abi ARMEABI-V7A --splashscreen-icon
+      if [ $? -eq 0 ]; then
+         cmd notification post -S bigtext -t 'MLQL · Laxeron' 'Executed' 'Starting Mobile Legends with Armeabi-v7a !' > /dev/null 2>&1 &
+     else
+         echo "[ Can't start app or Error ! ]"
+     fi
+else
+    am start -D -N -S --user 0 "${id[0]}" --es --windowingMode 1 --no-window-animation
+       if [ $? -eq 0 ]; then
+         cmd notification post -S bigtext -t 'MLQL · Laxeron' 'Executed' 'Starting APP, Enjoy your games !' > /dev/null 2>&1 &
+     else
+         echo "[ Can't start app or Error ! ]"
+     fi
+fi
+}
+launch=true
 
 echo "[ Game Discovered as > $game ]"
 echo ""
