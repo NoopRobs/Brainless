002c6f6ecb533518a25dc16d5ede0c8ead370648
NoopRobs <150570960+NoopRobs@users.noreply.github.com>
2024-07-09 23:47:09 +0700

    Update Main.sh

--- Main.sh
+++ Main.sh
@@ -31,14 +31,14 @@ height=$(echo $size | cut -d 'x' -f 2)
 
 launch_app () {
 if ls /sdcard/android/data/$game/files/dragon2017/assets/comlibs/armeabi-v7a; then
-   am start -D -N -S --user 0 "${id[0]}" --es --windowingMode 1 --no-window-animation --abi ARMEABI-V7A --splashscreen-icon
+   am start -S --user 0 "${id[0]}" --es --windowingMode 1 --no-window-animation --abi ARMEABI-V7A --splashscreen-icon
       if [ $? -eq 0 ]; then
          cmd notification post -S bigtext -t 'MLQL · Laxeron' 'Executed' 'Starting Mobile Legends with Armeabi-v7a !' > /dev/null 2>&1 &
      else
          echo "[ Can't start app or Error ! ]"
      fi
 else
-    am start -D -N -S --user 0 "${id[0]}" --es --windowingMode 1 --no-window-animation
+    am start -S --user 0 "${id[0]}" --es --windowingMode 1 --no-window-animation
        if [ $? -eq 0 ]; then
          cmd notification post -S bigtext -t 'MLQL · Laxeron' 'Executed' 'Starting APP, Enjoy your games !' > /dev/null 2>&1 &
      else
