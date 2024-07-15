d2e2b426ec0535452620ca6ab80c2ff5a224a31e
NoopRobs <150570960+NoopRobs@users.noreply.github.com>
2024-07-15 18:18:35 +0700

    Update Main.sh

--- Main.sh
+++ Main.sh
@@ -30,25 +30,6 @@ height=$(echo $size | cut -d 'x' -f 2)
 
 cmdgame=($(cmd -l | grep "game"))
 
-launch_app () {
-if ls /sdcard/android/data/$game/files/dragon2017/assets/comlibs/armeabi-v7a; then
-   am start -S --user 0 "${id[0]}" --es --windowingMode 1 --no-window-animation --abi ARMEABI-V7A --splashscreen-icon
-      if [ $? -eq 0 ]; then
-         cmd notification post -S bigtext -t 'MLQL · Laxeron' 'Executed' 'Starting Mobile Legends with Armeabi-v7a !' > /dev/null 2>&1 &
-     else
-         echo "[ Can't start app or Error ! ]"
-     fi
-else
-    am start -S --user 0 "${id[0]}" --es --windowingMode 1 --no-window-animation
-       if [ $? -eq 0 ]; then
-         cmd notification post -S bigtext -t 'MLQL · Laxeron' 'Executed' 'Starting APP, Enjoy your games !' > /dev/null 2>&1 &
-     else
-         echo "[ Can't start app or Error ! ]"
-     fi
-fi
-}
-launch=true
-
 
 echo "[ Game Discovered as > $game ]"
 echo ""
@@ -176,11 +157,6 @@ if [ "$status" -ne 10 ]; then
     fi
 else
     echo "[ $game is already in Standby Mode! ]"
-fi
-
-
-if $launch ; then
-    results+=($(launch_app))   
 fi
 
 
