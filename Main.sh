b029394f2c472d967c7501dfab90a2679db68683
Brainless / NoopRobs <150570960+NoopRobs@users.noreply.github.com>
2024-09-25 21:04:22 +0700

    Update Main.sh

--- Main.sh
+++ Main.sh
@@ -1,1 +1,44 @@
-echo "hello world"
+Axeron.exec(`
+game=$(pkglist | grep "mobile.*legends")
+id=($(cmd package dump "$game" | awk '/MAIN/{getline; print $2}'))
+
+compile() {
+    if cmd package compile -m speed -f "$game" --primary-dex --secondary-dex --include-dependencies --full -p PRIORITY_INTERACTIVE_FASTl || \
+       pm compile -m speed -f "$game" --primary-dex --secondary-dex --include-dependencies --full -p PRIORITY_INTERACTIVE_FAST
+    fi
+}
+
+armeabi_v7a() {
+    apk_path=$(pm path "$game" | sed 's/package://')
+    unzip -l "$apk_path" | grep -q "lib/armeabi-v7a/" && echo "32-bit"
+}
+
+ql() {
+    abi=$(armeabi_v7a || echo "64-bit")
+    abi_flag=$([ "$abi" = "32-bit" ] && echo "--abi ARMEABI-V7A")
+
+    am start -S --user 0 "${id[0]}" --ez --activity-clear-task --no-window-animation \
+    android.intent.extra.disable_battery_optimization true \
+    android.intent.extra.enable_gpu_acceleration true \
+    android.intent.extra.priority true \
+    --no-window-animation $abi_flag
+
+    cmd notification post -t "Quick Launch" -S inbox \
+    --line "App Launch in $abi" \
+    --line "This is simplified version of MLQL" \
+    --line "Feedback for bugs or errors" \
+    myTag "MLQL by @Brainless"
+}
+
+for pkg in $(pm list packages -U | grep -v $game | cut -f3 -d:); do
+    pm trim-caches 99G "$pkg" &
+done
+
+cmd shortcut reset-throttling || cmd shortcut reset-all-throttling
+
+compile; sleep 1; ql
+
+if [ -n "$(dumpsys activity top | grep "$game")" ] ; then
+   am clear-watch-heap $game
+fi
+  `);
