7d180d82cf778c7f2209dc923b7cba9a3a7b91c2
Brainless / NoopRobs <150570960+NoopRobs@users.noreply.github.com>
2024-10-04 13:23:27 +0700

    Update gmode.sh

--- gmode.sh
+++ gmode.sh
@@ -11,14 +11,9 @@ device_config put game_overlay $game mode=2,downscaleFactor=0.3,fps=120
 
 # enable VULKAN If possible
 # Will be Update further next time
-current_apps=$(settings get global updatable_driver_production_opt_in_apps)
-IFS=',' read -r -a apps_array <<< "$current_apps"
+a=$(settings get global updatable_driver_production_opt_in_apps)
 
-echo "Apps saat ini: ${apps_array[@]}"
-
-new_package="game"
-apps_array+=("$new_package")
-updated_apps=$(IFS=','; echo "${apps_array[*]}")
-
-settings put global updatable_driver_production_opt_in_apps "$updated_apps"
-echo "Updated apps: $updated_apps"
+# Menambahkan com.discord ke dalam daftar aplikasi, jika belum ada
+if [[ "$a" != *"$game"* ]]; then
+    settings put global updatable_driver_production_opt_in_apps "$a,com.discord"
+fi
