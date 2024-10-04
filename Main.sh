952fc4a9e2fe5941010088b0c7b805ee98be5304
Brainless / NoopRobs <150570960+NoopRobs@users.noreply.github.com>
2024-10-04 23:55:36 +0700

    Update Main.sh
    notif up

--- Main.sh
+++ Main.sh
@@ -31,8 +31,16 @@ launch_app() {
 # Inside ql function
 if [ ${#abi[@]} -gt 0 ]; then
     launch_app --abi ARMEABI-V7A
+    cmd notification post -t "Quick Launch" -S inbox \
+    --line "App Running in 32-bit" \
+    --line "Feedback for bugs or errors" \
+    myTag "Quick Launch - Brainless"
 else
     launch_app
+    cmd notification post -t "Quick Launch" -S inbox \
+    --line "App Running in 64-bit" \
+    --line "Feedback for bugs or errors" \
+    myTag "Quick Launch - Brainless"
 fi
 
 if [ -n "$(dumpsys activity top | grep "$game")" ] ; then
