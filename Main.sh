88b5ace4288304a554855abd8816efefc3daad50
Brainless / NoopRobs <150570960+NoopRobs@users.noreply.github.com>
2024-10-04 16:03:40 +0700

    Update Main.sh
    new method abi check

--- Main.sh
+++ Main.sh
@@ -6,15 +6,31 @@ compile() {
 }
      
 ql() {
-    abi=$(armeabi_v7a || echo "64-bit")
+    # Detect ABI
+    apk_path=$(pm path $game | sed 's/package://g')
+    armeabi_v7a_apps=()
+
+    if [ -n "$apk_path" ]; then
+        if unzip -l "$apk_path" | grep -q "lib/armeabi-v7a/"; then
+            abi="32-bit"
+            armeabi_v7a_apps+=("$game")
+        else
+            abi="64-bit"
+        fi
+    else
+        abi="64-bit"  # Default to 64-bit if apk path is not found
+    fi
+
     abi_flag=$([ "$abi" = "32-bit" ] && echo "--abi ARMEABI-V7A")
 
+    # Launch the app
     am start -S --user 0 "${id[0]}" --ez --activity-clear-task --no-window-animation \
     android.intent.extra.disable_battery_optimization true \
     android.intent.extra.enable_gpu_acceleration true \
     android.intent.extra.priority true \
     --no-window-animation $abi_flag
 
+    # Notification
     cmd notification post -t "Quick Launch" -S inbox \
     --line "App Running in $abi" \
     --line "Feedback for bugs or errors" \
