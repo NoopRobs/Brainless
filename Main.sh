4dee63aef3b5a20d669c8a5f6f3bd19854cc39ed
Brainless / NoopRobs <150570960+NoopRobs@users.noreply.github.com>
2024-10-04 23:48:46 +0700

    Update Main.sh
    huge funct fix (hopfully)

--- Main.sh
+++ Main.sh
@@ -1,6 +1,8 @@
 game="$MODULE_PKG"
 id=($(cmd package dump "$game" | awk '/MAIN/{getline; print $2}'))
 
+cmd shortcut reset-throttling || cmd shortcut reset-all-throttling
+
 compile() {
      cmd package compile -m speed -f "$game" --primary-dex --secondary-dex --include-dependencies --full -p PRIORITY_INTERACTIVE_FAST
 }
@@ -18,55 +20,20 @@ armeabi_v7a () {
     echo "${armeabi_v7a_apps[@]}"
 }
 
-ql () {
-    abi=($(armeabi_v7a))
-
-    if [ ${#abi[@]} -gt 0 ]; then
-        am start -S --user 0 "${id[0]}" --ez --activity-clear-task --no-window-animation \
+launch_app() {
+    am start -S --user 0 "${id[0]}" --ez --activity-clear-task --no-window-animation \
         android.intent.extra.disable_battery_optimization true \
         android.intent.extra.enable_gpu_acceleration true \
         android.intent.extra.priority true \
-    --no-window-animation --abi ARMEABI-V7A
-        if [ $? -eq 0 ]; then
-            cmd notification post -t "Quick Launch" -S inbox \
-            --line "App Running in 32-bit" \
-            --line "Feedback for bugs or errors" \
-            myTag "Quick Launch - Brainless"
-        else
-            am start -S --user 0 "${id[0]}" --ez --activity-clear-task --no-window-animation \
-            android.intent.extra.disable_battery_optimization true \
-            android.intent.extra.enable_gpu_acceleration true \
-            android.intent.extra.priority true \
-            --no-window-animation
-                if [ $? -eq 0 ]; then
-                    cmd notification post -t "Quick Launch" -S inbox \
-                    --line "App Running in 64-bit" \
-                    --line "Feedback for bugs or errors" \
-                    myTag "Quick Launch - Brainless"
-                fi
-        fi
-    else
-        am start -S --user 0 "${id[0]}" --ez --activity-clear-task --no-window-animation \
-        android.intent.extra.disable_battery_optimization true \
-        android.intent.extra.enable_gpu_acceleration true \
-        android.intent.extra.priority true \
-        --no-window-animation
-            if [ $? -eq 0 ]; then
-                cmd notification post -t "Quick Launch" -S inbox \
-                --line "App Running in 64-bit" \
-                --line "Feedback for bugs or errors" \
-                myTag "Quick Launch - Brainless"
-            fi
-    fi
+        --no-window-animation "$@"
 }
 
-for pkg in $(pm list packages -U | grep -v $game | cut -f3 -d:); do
-    pm trim-caches 99G "$pkg" &
-done
-
-cmd shortcut reset-throttling || cmd shortcut reset-all-throttling
-
-sleep 1; ql
+# Inside ql function
+if [ ${#abi[@]} -gt 0 ]; then
+    launch_app --abi ARMEABI-V7A
+else
+    launch_app
+fi
 
 if [ -n "$(dumpsys activity top | grep "$game")" ] ; then
    am clear-watch-heap $game
