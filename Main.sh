4a4c08a660186acf1cf1d0671c28f8793383855d
Brainless / NoopRobs <150570960+NoopRobs@users.noreply.github.com>
2024-10-05 20:38:36 +0700

    Main.sh
    major update

--- Main.sh
+++ Main.sh
@@ -4,42 +4,29 @@ id=($(cmd package dump "$game" | awk '/MAIN/{getline; print $2}'))
 cmd shortcut reset-throttling || cmd shortcut reset-all-throttling
 
 compile() {
-     cmd package compile -m speed -f "$game" --primary-dex --secondary-dex --include-dependencies --full -p PRIORITY_INTERACTIVE_FAST
+     cmd package compile -m speed-profile -f "$game" --primary-dex --secondary-dex --include-dependencies --full -p PRIORITY_INTERACTIVE_FAST
 }
      
-armeabi_v7a () {
-    apk_path=$(pm path $game | sed 's/package://g')
-    armeabi_v7a_apps=()
+compile; sleep 0,5
 
-    if [ -n "$apk_path" ]; then
-        if unzip -l "$apk_path" | grep -q "lib/armeabi-v7a/"; then
-            armeabi_v7a_apps+=("$game")
-        fi
-    fi
-
-    echo "${armeabi_v7a_apps[@]}"
-}
-abi=($(armeabi_v7a))
-
-launch_app() {
-    am start -S --user 0 "${id[0]}" --ez --activity-clear-task --no-window-animation \
-        android.intent.extra.disable_battery_optimization true \
-        android.intent.extra.enable_gpu_acceleration true \
-        android.intent.extra.priority true \
-        --no-window-animation "$@"
+ql () {
+am start -S --user 0 "${id[0]}" --ez --activity-clear-task --no-window-animation \
+android.intent.extra.disable_battery_optimization true \
+android.intent.extra.enable_gpu_acceleration true \
+android.intent.extra.priority true \
+--no-window-animation
 }
 
-# Inside ql function
-if [ ${#abi[@]} -gt 0 ]; then
-    launch_app --abi ARMEABI-V7A
+ql --abi ARMEABI_V7A
+if [ $? -eq 0 ]; then
     cmd notification post -t "Quick Launch" -S inbox \
     --line "App Running in 32-bit" \
     --line "Feedback for bugs or errors" \
     myTag "Quick Launch - Brainless"
 else
-    launch_app
+    ql
     cmd notification post -t "Quick Launch" -S inbox \
-    --line "App Running in 64-bit" \
+    --line "App Running in 32-bit" \
     --line "Feedback for bugs or errors" \
     myTag "Quick Launch - Brainless"
 fi
