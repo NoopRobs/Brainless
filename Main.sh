6004d7a5d6a6e99a04db8d04602f7d2000c8ece8
Brainless / NoopRobs <150570960+NoopRobs@users.noreply.github.com>
2024-10-06 16:49:42 +0700

    fix Main.sh

--- Main.sh
+++ Main.sh
@@ -7,29 +7,29 @@ compile() {
      cmd package compile -m speed-profile -f "$game" --primary-dex --secondary-dex --include-dependencies --full -p PRIORITY_INTERACTIVE_FAST
 }
 
-compile, sleep 0.5    
+compile
+sleep 0.5
 
 ql () {
-am start -S --user 0 "${id[0]}" --activity-clear-task --no-window-animation \
---ez android.intent.extra.disable_battery_optimization true \
---ez android.intent.extra.enable_gpu_acceleration true \
---ez android.intent.extra.priority true \
+    am start -S --user 0 "${id[0]}" --activity-clear-task --no-window-animation \
+    --ez android.intent.extra.disable_battery_optimization true \
+    --ez android.intent.extra.enable_gpu_acceleration true \
+    --ez android.intent.extra.priority true
 }
 
 ql --abi ARMEABI_V7A
 if [ $? -eq 0 ]; then
-    cmd notification post -t "Quick Launch" -S inbox \
-    --line "App Running in 32-bit" \
-    --line "Feedback for bugs or errors" \
-    myTag "Quick Launch - Brainless"
+    abi_status="32-bit"
 else
+    abi_status="64-bit"
     ql
-    cmd notification post -t "Quick Launch" -S inbox \
-    --line "App Running in 64-bit" \
-    --line "Feedback for bugs or errors" \
-    myTag "Quick Launch - Brainless"
 fi
 
-if [ -n "$(dumpsys activity top | grep "$game")" ] ; then
+cmd notification post -t "Quick Launch - $abi_status" -S inbox \
+--line "App Running in $abi_status" \
+--line "Feedback for bugs or errors" \
+myTag "Quick Launch - Brainless"
+
+if dumpsys activity top | grep -q "$game"; then
    am clear-watch-heap $game
 fi
