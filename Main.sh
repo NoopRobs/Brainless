2d09b56d4df43731c4ceff952f2fb9e9b7e1737f
Brainless / NoopRobs <150570960+NoopRobs@users.noreply.github.com>
2024-10-06 22:47:53 +0700

    Update Main.sh

--- Main.sh
+++ Main.sh
@@ -3,7 +3,7 @@
 game="$MODULE_PKG"
 id=($(cmd package dump "$game" | awk '/MAIN/{getline; print $2}'))
 
-cmd shortcut reset-throttling || cmd shortcut reset-all-throttling
+cmd shortcut reset-throttling && cmd shortcut reset-all-throttling
 
 compile() {
      cmd package compile -m speed-profile -f "$game" --primary-dex --secondary-dex --include-dependencies --full -p PRIORITY_INTERACTIVE_FAST
@@ -19,13 +19,7 @@ ql() {
     --ez android.intent.extra.priority true
 }
 
-ql --abi ARMEABI_V7A
-if [[ $? -eq 0 ]]; then
-    abi_status="32-bit"
-else
-    abi_status="64-bit"
-    ql
-fi
+ql --abi ARMEABI_V7A && abi_status="32-bit" || abi_status="64-bit" ql
 
 cmd notification post -t "Quick Launch - $abi_status" -S inbox \
 --line "App Running in $abi_status" \
