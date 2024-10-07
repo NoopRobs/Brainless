ca46696c90e870f7fc25d567b0191f1c24c0ff8d
Brainless / NoopRobs <150570960+NoopRobs@users.noreply.github.com>
2024-10-07 13:52:33 +0700

    Update Main.sh

--- Main.sh
+++ Main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 game="$MODULE_PKG"
-id=($(cmd package dump "$game" | awk '/MAIN/{getline; print $2}'))
+id=($(pm dump $game | grep -o "$game/[^ ]*" | grep -i "main" | sort -u | head -n 1))
 
 cmd shortcut reset-throttling && cmd shortcut reset-all-throttling
 
