3526736a9243b8778936b002d3003bf3ebda001d
Brainless / NoopRobs <150570960+NoopRobs@users.noreply.github.com>
2024-10-08 10:22:18 +0700

    Update gmode.sh

--- gmode.sh
+++ gmode.sh
@@ -2,6 +2,7 @@ game="$MODULE_PKG"
 
 device_config put game_overlay $game mode=2,downscaleFactor=0.3,fps=120,useAngle=true,LoadingBoost=1
 
+
   if [ -z cmd -l | grep -wo "game"]; then
       echo "empty"
   else
@@ -9,11 +10,29 @@ device_config put game_overlay $game mode=2,downscaleFactor=0.3,fps=120,useAngle
   fi
 
 
-# enable VULKAN If possible
-# Will be Update further next time
 a=$(settings get global updatable_driver_production_opt_in_apps)
 
-# Menambahkan com.discord ke dalam daftar aplikasi, jika belum ada
+
+if [[ ls /system/lib/libvulkan.so ]]; then
+   setprop debug.hwui.renderer skiavk
+   setprop debug.renderengine.backend skiavkthreaded
+   setprop debug.angle.overlay FPS:SkiaVK*PipelineCache*
+fi
+
+
 if [[ "$a" != *"$game"* ]]; then
     settings put global updatable_driver_production_opt_in_apps "$a,com.discord"
 fi
+
+
+dumpsys deviceidle whitelist | grep -p "$game" || dumpsys deviceidle whitelist +"$game"
+
+
+cmd looper_stats disable
+cmd power set-adaptive-power-saver-enabled false
+cmd power set-fixed-performance-mode-enabled true
+cmd power set-mode 0
+cmd thermalservice override-status 0
+dumpsys deviceidle enable
+dumpsys deviceidle force-idle
+dumpsys deviceidle step deep
