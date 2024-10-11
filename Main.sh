fbe20a4d6622e6563ba0ca0eb7491397b6ccdabc
Brainless / NoopRobs <150570960+NoopRobs@users.noreply.github.com>
2024-10-12 01:22:42 +0700

    Update Main.sh

--- Main.sh
+++ Main.sh
@@ -8,19 +8,14 @@ cmd shortcut reset-throttling && cmd shortcut reset-all-throttling
 cmd package compile -m speed-profile -f "$game" --primary-dex --secondary-dex --include-dependencies --full -p PRIORITY_INTERACTIVE_FAST
 
 ql() {
-    am start -S --user 0 "${id[0]}" --activity-clear-task --no-window-animation \
+    am start -S --user 0 "${id[0]}" --activity-clear-task --no-window-animation --splashscreen_icon \
     --ez android.intent.extra.disable_battery_optimization true \
     --ez android.intent.extra.enable_gpu_acceleration true \
     --ez android.intent.extra.priority true
 }
 
 cache_dir="/data/data/$game/cache"
-if [[ -d "$cache_dir" ]]; then
-    rm -rf $cache_dir/*
-    log "Cache cleared for $game."
-else
-    log "No cache directory found for $game."
-fi
+-d "$cache_dir" && rm -rf $cache_dir/* log "Cache cleared for $game." || log "No cache directory found for $game."
 
 ql --abi ARMEABI_V7A && abi_status="32-bit" || abi_status="64-bit" ql
 
@@ -29,6 +24,4 @@ cmd notification post -t "Quick Launch - $abi_status" -S inbox \
 --line "Feedback for bugs or errors" \
 myTag "Quick Launch - Brainless"
 
-if [[ dumpsys activity top | grep -q "$game" ]]; then
-   am clear-watch-heap $game
-fi
+dumpsys activity top | grep -q "$game" && am clear-watch-heap $game
