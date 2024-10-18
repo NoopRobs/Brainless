4a6006978a182c5d15c6b6c8a87ecbe096449b70
Brainless / NoopRobs <150570960+NoopRobs@users.noreply.github.com>
2024-10-18 10:36:25 +0700

    Update Main.sh
    add some flags into am start function

--- Main.sh
+++ Main.sh
@@ -8,10 +8,13 @@ cmd shortcut reset-throttling && cmd shortcut reset-all-throttling
 cmd package compile -m speed-profile -f "$game" --primary-dex --secondary-dex --include-dependencies --full -p PRIORITY_INTERACTIVE_FAST
 
 ql() {
-    am start -S --user 0 "${id[0]}" --activity-clear-task --no-window-animation \
+    am start -S --user 0 "${id[0]}" --activity-clear-task --no-window-animation --activity-reorder-to-front \
     --ez android.intent.extra.disable_battery_optimization true \
     --ez android.intent.extra.enable_gpu_acceleration true \
-    --ez android.intent.extra.priority true
+    --ez android.intent.extra.priority true \
+    --ez android.intent.extra.ALLOW_IDLE_MODE true \
+    --ez android.intent.extra.low_ram true \
+    --ez android.intent.extra.allow_background_activity_start false
 }
 
 ql --abi ARMEABI_V7A && abi_status="32-bit" || abi_status="64-bit" ql
