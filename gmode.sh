5a833c7ad627f8ca615419ac92c6af9c514afdbe
Brainless / NoopRobs <150570960+NoopRobs@users.noreply.github.com>
2024-11-03 10:16:56 +0700

    Update gmode.sh

--- gmode.sh
+++ gmode.sh
@@ -18,16 +18,14 @@ if [[ "$a" != *"$game"* ]]; then
 fi
 
 
-dumpsys deviceidle whitelist | grep -p "$game" || dumpsys deviceidle whitelist +"$game"
+device_config put activity_manager_native_boot use_freezer true
 
 
-cmd looper_stats | grep -q "Looper stats disabled" || { echo "Disabling looper stats..."; cmd looper_stats disable; }
-cmd power get-adaptive-power-saver-enabled | grep -q "false" || { echo "Setting adaptive power saver to false..."; cmd power set-adaptive-power-saver-enabled false; }
-cmd power get-fixed-performance-mode-enabled | grep -q "true" || { echo "Enabling fixed performance mode..."; cmd power set-fixed-performance-mode-enabled true; }
-cmd power get-mode | grep -q "0" || { echo "Setting power mode to 0..."; cmd power set-mode 0; }
-cmd thermalservice get-override-status | grep -q "0" || { echo "Overriding thermal status to 0..."; cmd thermalservice override-status 0; }
+cmd power set-adaptive-power-saver-enabled false
+cmd power set-fixed-performance-mode-enabled true
+cmd power set-mode 0
+cmd thermalservice override-status 0
+cmd looper_stats disable
 
 
-dumpsys deviceidle | grep -q "mLightEnabled=true" || { echo "Enabling device idle..."; dumpsys deviceidle enable; }
-dumpsys deviceidle | grep -q "mForceIdle=true" || { echo "Activating forced idle mode..."; dumpsys deviceidle force-idle; }
-dumpsys deviceidle step | grep -q "Light" || { echo "Stepping into light state..."; dumpsys deviceidle step d; }
+cmd dropbox remove-low-priority "$game"
